78e75eba3827001f65cbf688fcd721f4f77c1e29
Jung ChangHoon <chjung@euclidsoft.co.kr>
2022-03-28 13:14:56 +0900

    Create setting.bat (세팅 배치 프로그램 추가)

---
+++ setting.bat
@@ -0,0 +1,32 @@
+echo 'setting...'
+
+rem 모두 yes 입력!
+
+call conda activate base
+
+rem 특정 위치에 저장할 때: -p 옵션 적용 (주석[rem] 해제하고 path만 바꿔 배치 돌려주세요.)
+set venv_path=C:\esb\envs\krict
+call conda env remove --prefix %venv_path%
+call conda create -c conda-forge --prefix %venv_path% python=3.8.12 rdkit
+call conda activate %venv%
+
+rem 디폴트 위치에 저장할 때: -n 옵션 적용. (주석[rem] 해제하고 path만 바꿔 배치 돌려주세요.)
+rem set venv=krict
+rem call conda env remove -n %venv%
+rem call conda create -c conda-forge -n %venv% python=3.8.12 rdkit
+rem call conda activate %venv%
+
+call pip install django
+call pip install django_crontab
+
+call pip install mysqlclient
+
+call pip install b4
+call pip install -r requirements.txt
+
+echo 'Done.'
+
+rem 필요시 포트 변경
+call python manage.py makemigrations
+call python manage.py migrate
+call python manage.py runserver 8080
