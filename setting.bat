20f36b856eee4c03df57af7a7270241538abad9b
Jung ChangHoon <chjung@euclidsoft.co.kr>
2022-03-29 10:12:36 +0900

    Modify setting.bat

--- setting.bat
+++ setting.bat
@@ -10,7 +10,7 @@ call conda env remove --prefix %venv_path%
 call conda create -c conda-forge --prefix %venv_path% python=3.8.12 rdkit
 call conda activate %venv_path%
 
-rem 디폴트 위치에 저장할 때: -n 옵션 적용. (주석[rem] 해제하고 path만 바꿔 배치 돌려주세요.)
+rem 디폴트 위치에 저장할 때: -n 옵션 적용 (주석[rem] 해제하고 path만 바꿔 배치 돌려주세요.)
 rem set venv=krict
 rem call conda env remove -n %venv%
 rem call conda create -c conda-forge -n %venv% python=3.8.12 rdkit
