32467c28fc01e6f18c9eebdb9f14a2fcc895ea6a
Jung ChangHoon <chjung@euclidsoft.co.kr>
2022-04-04 09:23:42 +0900

    템플릿 태그 추가

--- job_user/templatetags/html_extras.py
+++ job_user/templatetags/html_extras.py
@@ -2,3 +2,21 @@ from django import template
 
 register = template.Library()
 
+
+@register.filter(name='get_job_status_btn_class')
+def get_job_status_btn_class(job_status):
+    if job_status == 'complete':
+        return 'button-status completed'
+    elif job_status == 'waiting':
+        return 'button-status waiting'
+    elif job_status == 'running':
+        return 'button-status running'
+    elif job_status == 'fail':
+        return 'button-status fail'
+    elif job_status == 'stop':
+        return 'button-status stop'
+
+
+@register.filter(name='subtract')
+def subtract(value, arg):
+    return value - arg
